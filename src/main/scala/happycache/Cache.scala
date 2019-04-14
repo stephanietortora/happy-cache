06f8dc7a8a18743de49a8350f9215150b35444ca
Stephanie <stephanie.a.tortora@gmail.com>
2019-04-14 15:02:08 -0400

    cleaning up

--- src/main/scala/happycache/Cache.scala
+++ src/main/scala/happycache/Cache.scala
@@ -1,17 +1,21 @@
 package happycache
+import collection.immutable.SortedSet
 
-trait CacheEntry[K, V] extends Ordered[CacheEntry[K,V]] {
+trait CacheEntry[K, V] extends Ordered[CacheEntry[K, V]] {
   val key: K
   val value: V
   val timeUpdated: Long
 
-  implicit def compare(that: CacheEntry[K,V]): Int
-  def apply(key: K, value: V, timeUpdated: Long): CacheEntry[K,V]
-  def copy(key: K = this.key, value: V = this.value, timeUpdated: Long = this.timeUpdated): CacheEntry[K,V]
+  implicit def compare(that: CacheEntry[K, V]): Int
+
+  def apply(key: K, value: V, timeUpdated: Long): CacheEntry[K, V]
+
+  def copy(key: K = this.key, value: V = this.value, timeUpdated: Long = this.timeUpdated): CacheEntry[K, V]
 }
 
 trait Cache[K, V] {
   def get(k: K): Option[V]
+
   def put(k: K, v: V): Unit
 }
 
@@ -19,16 +23,18 @@ object Cache {
 
   private case class CacheEntryTime[K, V](key: K, value: V, timeUpdated: Long) extends CacheEntry[K, V] {
     def copy(key: K = this.key, value: V = this.value, timeUpdated: Long = this.timeUpdated): CacheEntryTime[K, V] = CacheEntryTime(key, value, timeUpdated)
-    override def apply(key: K, value: V, timeUpdated: Long): CacheEntry[K, V] = CacheEntryTime(key, value, timeUpdated).asInstanceOf[CacheEntry[K,V]]
+
+    override def apply(key: K, value: V, timeUpdated: Long): CacheEntry[K, V] = CacheEntryTime(key, value, timeUpdated).asInstanceOf[CacheEntry[K, V]]
+
     implicit def compare(that: CacheEntry[K, V]): Int = this.timeUpdated.compareTo(that.timeUpdated)
   }
 
   private class CacheImpl[K, V]
   (numSet: Int,
    numEntry: Int,
-   update: (collection.immutable.SortedSet[CacheEntry[K,V]], CacheEntry[K,V]) => (collection.immutable.SortedSet[CacheEntry[K,V]], CacheEntry[K,V]),
-   newEntry: (K, V, Long) => CacheEntry[K,V])(implicit ordering: Ordering[CacheEntry[K,V]]) extends Cache[K, V] {
-    type T = CacheEntry[K,V]
+   update: (collection.immutable.SortedSet[CacheEntry[K, V]], CacheEntry[K, V]) => (collection.immutable.SortedSet[CacheEntry[K, V]], CacheEntry[K, V]),
+   newEntry: (K, V, Long) => CacheEntry[K, V])(implicit ordering: Ordering[CacheEntry[K, V]]) extends Cache[K, V] {
+    type T = CacheEntry[K, V]
 
     private var cache = Vector.fill(numSet) {
       (collection.immutable.HashMap.empty[K, T], collection.immutable.SortedSet.empty[T])
@@ -64,25 +70,32 @@ object Cache {
     }
   }
 
-  def mruCache[K, V](numSet: Int, numEntry: Int)(implicit ordering: Ordering[CacheEntry[K,V]]):Cache[K,V] = {
-    def newEntry: (K, V, Long) => CacheEntry[K,V] = (k: K, v: V, l: Long) => CacheEntryTime(k, v, l)
 
-    def update(entries: collection.immutable.SortedSet[CacheEntry[K,V]], toReplace: CacheEntry[K,V]): (collection.immutable.SortedSet[CacheEntry[K,V]], CacheEntry[K,V]) = {
+  def mruCache[K, V](numSet: Int, numEntry: Int)(implicit ordering: Ordering[CacheEntry[K, V]]): Cache[K, V] = {
+    def update(entries: collection.immutable.SortedSet[CacheEntry[K, V]], toReplace: CacheEntry[K, V]): (collection.immutable.SortedSet[CacheEntry[K, V]], CacheEntry[K, V]) = {
       (entries.init + toReplace, entries.last)
 
     }
-    new CacheImpl[K, V](numSet, numEntry, update, newEntry)
+    cache(numSet, numEntry, update)
   }
 
-  def lruCache[K, V](numSet: Int, numEntry: Int)(implicit ordering: Ordering[CacheEntry[K,V]]):Cache[K,V] = {
-    def newEntry: (K, V, Long) => CacheEntry[K,V] = (k: K, v: V, l: Long) => CacheEntryTime(k, v, l)
+  def lruCache[K, V](numSet: Int, numEntry: Int)(implicit ordering: Ordering[CacheEntry[K, V]]): Cache[K, V] = {
+    def update(entries: collection.immutable.SortedSet[CacheEntry[K, V]], toReplace: CacheEntry[K, V]): (collection.immutable.SortedSet[CacheEntry[K, V]], CacheEntry[K, V]) = {
+      (entries.tail + toReplace, entries.head)}
 
-    def update(entries: collection.immutable.SortedSet[CacheEntry[K,V]], toReplace: CacheEntry[K,V]): (collection.immutable.SortedSet[CacheEntry[K,V]], CacheEntry[K,V]) = {
-      (entries.tail + toReplace, entries.head)
-
-    }
-    new CacheImpl[K, V](numSet, numEntry, update, newEntry)
+    cache(numSet, numEntry, update)
   }
+
+  def cache[K, V](numSet: Int,
+                  numEntry: Int,
+                  update: (SortedSet[CacheEntry[K, V]], CacheEntry[K, V]) => (SortedSet[CacheEntry[K, V]], CacheEntry[K, V]))
+                 (implicit ordering: Ordering[CacheEntry[K, V]]): Cache[K, V] = cache(numSet, numEntry, update, (k,v,l) => CacheEntryTime(k, v, l))
+
+  def cache[K, V](numSet: Int,
+                  numEntry: Int,
+                  update: (SortedSet[CacheEntry[K, V]], CacheEntry[K, V]) => (SortedSet[CacheEntry[K, V]], CacheEntry[K, V]),
+                  newEntry: (K, V, Long) => CacheEntry[K, V])
+                 (implicit ordering: Ordering[CacheEntry[K, V]]): Cache[K, V] = new CacheImpl[K, V](numSet, numEntry, update, newEntry)
 
 }
 
